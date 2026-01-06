e502f41b15bbcca87ccf07f8ce029e322694d9c1
punit1808 <112548386+punit1808@users.noreply.github.com>
2026-01-06 17:46:08 +0530

    env update

--- constants/env.ts
+++ constants/env.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://location-sharing-backend-zwxt.onrender.com";
-export const WEBSOCKET_URL = "wss://location-sharing-backend-zwxt.onrender.com/ws/location";
+export const API_URL = "//Your backend URL";
+export const WEBSOCKET_URL = "//Your websocket URL";
